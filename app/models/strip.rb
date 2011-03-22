99779fde76a5298dfe0b77fac52abc63065d035f
Fabian Schlenz <mail@fabianonline.de>
2011-03-22 12:52:21 +0100

    Stripauswahl jetzt über ein NamedScope: Übergibt man ihm das heutige Datum, sucht er alle strips zwischen gestern, 12 Uhr und heute, 12 Uhr.

--- app/models/strip.rb
+++ app/models/strip.rb
@@ -1,5 +1,10 @@
 class Strip < ActiveRecord::Base
   belongs_to :comic
+  named_scope :by_date, lambda {|date| 
+    start = date.to_time - 12.hours;
+    ende = start + 24.hours - 1;
+    {:conditions=>["date>=? AND date<?", start, ende]}
+  }
 
   def filepath
     File.join("/", "comics", comic.directory, filename)
